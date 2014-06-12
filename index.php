ccec1d9b40bf416d74cecad7228b30d81557512e
Scott Deca <decasc@gmail.com>
2014-06-11 22:29:33 -0400

    styled the bullets

--- index.php
+++ index.php
@@ -46,28 +46,28 @@
 	</div>
 	
 	<div class="bullets">
-	  <div class="bullet four-col-bullet">
+	  <div class="bullet">
 	    <div class="bullet-icon bullet-icon-1"></div>
 	    <div class="bullet-content">
 	      <h2>This Bullet Title</h2>
 	      <p>Lorem dolor sit amet consectetur adipisicing elit. Doloremque, minus, blanditiis, voluptatibus nulla quia ipsam sequi quos iusto aliquam iste magnam accusamus molestias quo illum impedit. Odit officia autem.</p>
 	      </div>
 	  </div>  
-	  <div class="bullet four-col-bullet">
+	  <div class="bullet">
 	    <div class="bullet-icon bullet-icon-2"></div>
 	    <div class="bullet-content">
 	      <h2>Another Bullet Title</h2>
 	      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, minus, blanditiis, voluptatibus nulla quia ipsam sequi quos iusto aliquam iste magnam accusamus molestias quo illum.</p>
 	    </div>
 	  </div>
-	  <div class="bullet four-col-bullet">
+	  <div class="bullet">
 	    <div class="bullet-icon bullet-icon-3"></div>
 	    <div class="bullet-content">
 	      <h2>Last Bullet Title</h2>
 	      <p>Lorem ipsum sit amet consectetur adipisicing elit. Doloremque, minus, blanditiis, voluptatibus nulla quia ipsam sequi quos iusto aliquam iste magnam accusamus molestias quo illum impedit. Odit officia autem.</p>
 	    </div>
 	  </div>
-	  <div class="bullet four-col-bullet">
+	  <div class="bullet">
 	    <div class="bullet-icon bullet-icon-4"></div>
 	    <div class="bullet-content">
 	      <h2>Last Bullet Title</h2>

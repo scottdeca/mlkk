a235912d5ed8c203b8997b0e63167594157a31ce
Scott Deca <decasc@gmail.com>
2014-06-11 21:03:44 -0400

    Tweaked the hero and topnav

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 	<meta charset="utf-8">
 	<title>Home - MLK Kitchen for the Poor</title>
 	<link rel="stylesheet" href="css/app.css">
+	
+	<script type="text/javascript" src="//use.typekit.net/vvd8hgv.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 </head>
 <body>
 
@@ -15,64 +18,59 @@
 	    <p class="navigation-menu-button" id="js-mobile-menu">MENU</p>
 	    <div class="nav">
 	      <ul id="navigation-menu">
-	        <li class="nav-link"><a href="javascript:void(0)">Products</a></li>
-	        <li class="nav-link"><a href="javascript:void(0)">About Us</a></li>
-	        <li class="nav-link"><a href="javascript:void(0)">Contact</a></li>
-	        <li class="nav-link more"><a href="javascript:void(0)">More</a>
+	        <li class="nav-link"><a href="javascript:void(0)">Our Work</a></li>
+	        <li class="nav-link"><a href="javascript:void(0)">Get Help</a></li>
+	        <li class="nav-link"><a href="javascript:void(0)">Get Involved</a></li>
+	        <li class="nav-link more"><a href="javascript:void(0)">News & Events</a>
 	          <ul class="submenu">
 	            <li><a href="javascript:void(0)">Submenu Item</a></li>
 	            <li><a href="javascript:void(0)">Another Item</a></li>
 	          </ul>
 	        </li>
+	        <li class="nav-link"><a href="javascript:void(0)">About Us</a></li>
+	        <li class="nav-link"><a href="javascript:void(0)">Contact</a></li>
 	      </ul>
-	    </div>
-	    <div class="navigation-tools">
-	      <div class="search-bar">
-	        <div class="search-and-submit">
-	          <input type="search" placeholder="Enter Search" />
-	          <button type="submit">
-	            <img src="images/search-icon.png" alt="">
-	          </button>
-	        </div>
-	      </div>
-	      <a href="javascript:void(0)" class="sign-up">Sign Up</a>
 	    </div>
 	  </div>
 	</header>
 
-  <div class="image-gradient">
-    <div class="copy">
-    <p>Fixed height container</p>
-    </div>
-    <div class="overlay"></div>
-  </div>
+	<div class="hero">
+		<div class="hero-inner">
+			<div class="hero-copy">
+				<h1>Tem qui dellor sunt voluptaepre 200 et a con 52 soluptati unt quo omniet occaesequo ommo.</h1>
+			</div>
+	    <button>Learn More</button>
+	    <button>Give</button>
+		</div>
+		<div class="overlay"></div>
+	</div>
 
-
+<script src="js/vendor/jquery.js"></script>
 
 <script type="text/javascript">
-$(document).ready(function() {
-  var menu = $('#navigation-menu');
-  var menuToggle = $('#js-mobile-menu');
-  var signUp = $('.sign-up');
-
-  $(menuToggle).on('click', function(e) {
-    e.preventDefault();
-    menu.slideToggle(function(){
-      if(menu.is(':hidden')) {
-        menu.removeAttr('style');
-      }
-    });
-  });
-
-  // underline under the active nav item
-  $(".nav .nav-link").click(function() {
-    $(".nav .nav-link").each(function() {
-      $(this).removeClass("active-nav-item");
-    });
-    $(this).addClass("active-nav-item");
-    $(".nav .more").removeClass("active-nav-item");
-  });
-});
+	$(document).ready(function() {
+	  var menu = $('#navigation-menu');
+	  var menuToggle = $('#js-mobile-menu');
+	  var signUp = $('.sign-up');
+	
+	  $(menuToggle).on('click', function(e) {
+	    e.preventDefault();
+	    menu.slideToggle(function(){
+	      if(menu.is(':hidden')) {
+	        menu.removeAttr('style');
+	      }
+	    });
+	  });
+	
+	  // underline under the active nav item
+	  $(".nav .nav-link").click(function() {
+	    $(".nav .nav-link").each(function() {
+	      $(this).removeClass("active-nav-item");
+	    });
+	    $(this).addClass("active-nav-item");
+	    $(".nav .more").removeClass("active-nav-item");
+	  });
+	});
 </script>
 
 </body>

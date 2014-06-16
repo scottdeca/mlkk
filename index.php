41092440a7344ba2c11117d6341db458c4e5578c
Scott Deca <decasc@gmail.com>
2014-06-16 02:03:52 -0400

    Featured page / newsfeed styling. Starting the testimonials

--- index.php
+++ index.php
@@ -1,180 +1,183 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Home - MLK Kitchen for the Poor</title>
-	<link rel="stylesheet" href="css/app.css">
-	
-	<script type="text/javascript" src="//use.typekit.net/vvd8hgv.js"></script>
-	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+  <meta charset="utf-8">
+  <title>Home - MLK Kitchen for the Poor</title>
+  <link rel="stylesheet" href="css/app.css">
+  
+  <script type="text/javascript" src="//use.typekit.net/vvd8hgv.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 </head>
 <body>
 
-	<header class="navigation">
-	  <div class="menu-wrapper">
-	    <a href="javascript:void(0)" class="logo">
-	      <img src="images/placeholder_logo_1.png" alt="">
-	    </a>
-	    <p class="navigation-menu-button" id="js-mobile-menu">MENU</p>
-	    <div class="nav">
-	      <ul id="navigation-menu">
-	        <li class="nav-link"><a href="javascript:void(0)">Our Work</a></li>
-	        <li class="nav-link"><a href="javascript:void(0)">Get Help</a></li>
-	        <li class="nav-link"><a href="javascript:void(0)">Get Involved</a></li>
-	        <li class="nav-link more"><a href="javascript:void(0)">News & Events</a>
-	          <ul class="submenu">
-	            <li><a href="javascript:void(0)">Submenu Item</a></li>
-	            <li><a href="javascript:void(0)">Another Item</a></li>
-	          </ul>
-	        </li>
-	        <li class="nav-link"><a href="javascript:void(0)">About Us</a></li>
-	        <li class="nav-link"><a href="javascript:void(0)">Contact</a></li>
-	      </ul>
-	    </div>
-	  </div>
-	</header>
+  <header class="navigation">
+    <div class="menu-wrapper">
+      <a href="javascript:void(0)" class="logo">
+        <img src="/img/kfp_logo_rev.png" alt="MLK Kitchen for the Poor">
+      </a>
+      <p class="navigation-menu-button" id="js-mobile-menu">MENU</p>
+      <div class="nav">
+        <ul id="navigation-menu">
+          <li class="nav-link"><a href="javascript:void(0)">Our Work</a></li>
+          <li class="nav-link"><a href="javascript:void(0)">Get Help</a></li>
+          <li class="nav-link"><a href="javascript:void(0)">Get Involved</a></li>
+          <li class="nav-link more"><a href="javascript:void(0)">News & Events</a>
+            <ul class="submenu">
+              <li><a href="javascript:void(0)">Submenu Item</a></li>
+              <li><a href="javascript:void(0)">Another Item</a></li>
+            </ul>
+          </li>
+          <li class="nav-link"><a href="javascript:void(0)">About Us</a></li>
+          <li class="nav-link"><a href="javascript:void(0)">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </header>
 
-	<div class="hero">
-		<div class="hero-inner">
-			<div class="hero-copy">
-				<h1>Tem qui dellor sunt voluptaepre 200 et a con 52 soluptati unt quo omniet occaesequo ommo.</h1>
-			</div>
-	    <button>Learn More</button>
-	    <button>Give</button>
-		</div>
-		<div class="overlay"></div>
-	</div>
-	
-	<section class="bullets">
-	  <div class="bullet">
-	    <div class="bullet-icon bullet-icon-1"></div>
-	    <div class="bullet-content">
-	      <h2>This Bullet Title</h2>
-	      <p>Lorem dolor sit amet consectetur adipisicing elit. Doloremque, minus, blanditiis, voluptatibus nulla quia ipsam sequi quos iusto aliquam iste magnam accusamus molestias quo illum impedit. Odit officia autem.</p>
-	      </div>
-	  </div>  
-	  <div class="bullet">
-	    <div class="bullet-icon bullet-icon-2"></div>
-	    <div class="bullet-content">
-	      <h2>Another Bullet Title</h2>
-	      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, minus, blanditiis, voluptatibus nulla quia ipsam sequi quos iusto aliquam iste magnam accusamus molestias quo illum.</p>
-	    </div>
-	  </div>
-	  <div class="bullet">
-	    <div class="bullet-icon bullet-icon-3"></div>
-	    <div class="bullet-content">
-	      <h2>Last Bullet Title</h2>
-	      <p>Lorem ipsum sit amet consectetur adipisicing elit. Doloremque, minus, blanditiis, voluptatibus nulla quia ipsam sequi quos iusto aliquam iste magnam accusamus molestias quo illum impedit. Odit officia autem.</p>
-	    </div>
-	  </div>
-	  <div class="bullet">
-	    <div class="bullet-icon bullet-icon-4"></div>
-	    <div class="bullet-content">
-	      <h2>Last Bullet Title</h2>
-	      <p>Lorem ipsum sit amet consectetur adipisicing elit. Doloremque, minus, blanditiis, voluptatibus nulla quia ipsam sequi quos iusto aliquam iste magnam accusamus molestias quo illum impedit. Odit officia autem.</p>
-	    </div>
-	  </div> 
-	</section>
-	
-	<section class="featuredpage-newsfeed">
-		<div class="featured-page">
-			<h2>Lorem Ipsum dolor mets</h2>
-			<p>Lorem ipsum sit amet consectetur adipisicing elit. Doloremque, minus, blanditiis, voluptatibus nulla quia ipsam sequi quos iusto aliquam iste magnam accusamus molestias quo illum impedit. Odit officia autem.</p>
-		</div>
-		<div class="newsfeed">
-			<ul>
-				<li>
-					<h3 class="newsfeed-headline">Toledo City Paper nominates MLK Kitchen for an award</h3>
-					<span class="newsfeed-meta">Mar 25, 2014 | <a href="#">Read More</a></span>
-				</li>
-				<li>
-					<h3 class="newsfeed-headline">Toledo City Paper nominates MLK Kitchen for an award</h3>
-					<span class="newsfeed-meta">Mar 25, 2014 | <a href="#">Read More</a></span>
-				</li>
-				<li>
-					<h3 class="newsfeed-headline">Toledo City Paper nominates MLK Kitchen for an award</h3>
-					<span class="newsfeed-meta">Mar 25, 2014 | <a href="#">Read More</a></span>
-				</li>
-			</ul>
-		</div>
-	</section>
-	
-	<section class="testimonials-homepage">
-		<h2 class="inline-with-rule">What people are saying about MLK Kitchen</h2>
-		<p>Tas dion recte nim re vendae rem quas molupta tioratur explit et eossiti quisit 
+  <div class="hero">
+    <div class="hero-inner">
+      <div class="hero-copy">
+        <h1>Tem qui dellor sunt voluptaepre 200 et a con 52 soluptati unt quo omniet occaesequo ommo.</h1>
+      </div>
+      <button>Learn More</button>
+      <button>Give</button>
+    </div>
+    <div class="overlay"></div>
+  </div>
+  
+  <section class="bullets">
+    <div class="bullet">
+      <div class="bullet-icon bullet-icon-1"></div>
+      <div class="bullet-content">
+        <h2>This Bullet Title</h2>
+        <p>Lorem dolor sit amet consectetur adipisicing elit. Doloremque, minus, blanditiis, voluptatibus nulla quia ipsam sequi quos iusto aliquam iste magnam accusamus molestias quo illum impedit. Odit officia autem.</p>
+        </div>
+    </div>  
+    <div class="bullet">
+      <div class="bullet-icon bullet-icon-2"></div>
+      <div class="bullet-content">
+        <h2>Another Bullet Title</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, minus, blanditiis, voluptatibus nulla quia ipsam sequi quos iusto aliquam iste magnam accusamus molestias quo illum.</p>
+      </div>
+    </div>
+    <div class="bullet">
+      <div class="bullet-icon bullet-icon-3"></div>
+      <div class="bullet-content">
+        <h2>Last Bullet Title</h2>
+        <p>Lorem ipsum sit amet consectetur adipisicing elit. Doloremque, minus, blanditiis, voluptatibus nulla quia ipsam sequi quos iusto aliquam iste magnam accusamus molestias quo illum impedit. Odit officia autem.</p>
+      </div>
+    </div>
+    <div class="bullet">
+      <div class="bullet-icon bullet-icon-4"></div>
+      <div class="bullet-content">
+        <h2>Last Bullet Title</h2>
+        <p>Lorem ipsum sit amet consectetur adipisicing elit. Doloremque, minus, blanditiis, voluptatibus nulla quia ipsam sequi quos iusto aliquam iste magnam accusamus molestias quo illum impedit. Odit officia autem.</p>
+      </div>
+    </div> 
+  </section>
+  
+  <section class="featuredpage-newsfeed">
+    <div class="featured-page">
+      <h2>Lorem Ipsum dolor mets</h2>
+      <p>The MLK Kitchen for the Poor was established in 1969 by the late Rev. Harvey Savage, Sr., in response to the tremendous need he saw in his community. Located in Toledo’s central city, the Kitchen feeds more than 200 people each day, from school children to the elderly.</p>
+      <button>Learn More</button>
+    </div>
+    <div class="newsfeed">
+      <ul>
+        <li>
+          <h3 class="newsfeed-headline">Toledo City Paper nominates MLK Kitchen for an award</h3>
+          <span class="newsfeed-meta">Mar 25, 2014 | <a class="read-more" href="#">Read More</a></span>
+        </li>
+        <li>
+          <h3 class="newsfeed-headline">Toledo City Paper nominates MLK Kitchen for an award</h3>
+          <span class="newsfeed-meta">Mar 25, 2014 | <a class="read-more" href="#">Read More</a></span>
+        </li>
+        <li>
+          <h3 class="newsfeed-headline">Toledo City Paper nominates MLK Kitchen for an award</h3>
+          <span class="newsfeed-meta">Mar 25, 2014 | <a class="read-more" href="#">Read More</a></span>
+        </li>
+      </ul>
+    </div>
+  </section>
+  
+  <section class="testimonials-homepage">
+    <h2 class="inline-with-rule">What people are saying about MLK Kitchen</h2>
+    <p class="description">Tas dion recte nim re vendae rem quas molupta tioratur explit et eossiti quisit 
 remfugit latem que pe. Ga. Uciliatur aut ut excepro vitatur remoluptus.</p>
-		
-		<div class="testimonial-widget">
-			<div class="testimonial-image">
-				<img src="http://placehold.it/300x250" alt="" />
-			</div>
-			<div class="testimonial-quote">
-				<p>Testimonial about the the kitchenipsum Tas dion recte nim re vendae rem quas molupta tioratur explit et eossiti quisit rem fugit latem que pe.</p>
-				<span class="testimonial-name">– Marc Reynolds</span>
-			</div>
-		</div>
-		<span class="read-more">Read more stories &rarr;</span>
-	</section>
-	
-	<footer>
-		<div class="colophon">
-			<img class="logo" src="/img/kfp_logo_rev.png" alt="MLK Kitchen for the Poor" />
-			<div class="contact-info">
-				<ul class="address">
-					<li>Street Address</li>
-					<li>Address #1</li>
-					<li>Toledo, OH 43604</li>
-				</ul>
-				<ul class="phone-email">
-					<li>419 555 5555</li>
-					<li>contact@kitchenforthepoor</li>
-				</ul>
-			</div>
-			
-			<p class="copyright">Copyright ©2014 Martin Luther King Kitchen for the Poor. <a href="#">Privacy Policy</a></p>
-		</div>
-		
-		<div class="tell-us-more">
-			<h3>Tell us about you</h3>
-			<p>Whether you’d like to help out at our next event, help cook some meals, or just learn more, we’d be glad to hear from you.</p>
-			<form>
-				<select name="select_element">
-					<option value="1">Option 1</option>
-					<option value="2">Option 2</option>
-					<option value="3">Option 3</option>
-				</select>
-				<input class="button" type="submit" value="submit">
-			</form>
-		</div>
-	</footer>
-	
+    
+    <div class="testimonial-widget">
+      <div class="testimonial-image">
+        <img src="http://placehold.it/300x250" alt="" />
+      </div>
+      <div class="testimonial-quote">
+        <p>Testimonial about the the kitchenipsum Tas dion recte nim re vendae rem quas molupta tioratur explit et eossiti quisit rem fugit latem que pe.</p>
+        <span class="testimonial-name">– Marc Reynolds</span>
+      </div>
+    </div>
+    <span class="read-more">Read more stories &rarr;</span>
+  </section>
+  
+  <footer>
+    <div class="grid-wrapper">
+      <div class="colophon">
+        <img class="logo" src="/img/kfp_logo_rev.png" alt="MLK Kitchen for the Poor">
+        <div class="contact-info">
+          <ul class="address">
+            <li>Street Address</li>
+            <li>Address #1</li>
+            <li>Toledo, OH 43604</li>
+          </ul>
+          <ul class="phone-email">
+            <li>419 555 5555</li>
+            <li>contact@kitchenforthepoor</li>
+          </ul>
+        </div>
+        
+        <p class="copyright">Copyright ©2014 Martin Luther King Kitchen for the Poor. <a href="#">Privacy Policy</a></p>
+      </div>
+      
+      <div class="tell-us-more">
+        <h3>Tell us about you</h3>
+        <p>Whether you’d like to help out at our next event, help cook some meals, or just learn more, we’d be glad to hear from you.</p>
+        <form>
+          <select name="select_element">
+            <option value="1">Option 1</option>
+            <option value="2">Option 2</option>
+            <option value="3">Option 3</option>
+          </select>
+          <input class="button" type="submit" value="submit">
+        </form>
+      </div>
+    </div>
+  </footer>
+  
 
 <script src="js/vendor/jquery.js"></script>
 
 <script type="text/javascript">
-	$(document).ready(function() {
-	  var menu = $('#navigation-menu');
-	  var menuToggle = $('#js-mobile-menu');
-	  var signUp = $('.sign-up');
-	
-	  $(menuToggle).on('click', function(e) {
-	    e.preventDefault();
-	    menu.slideToggle(function(){
-	      if(menu.is(':hidden')) {
-	        menu.removeAttr('style');
-	      }
-	    });
-	  });
-	
-	  // underline under the active nav item
-	  $(".nav .nav-link").click(function() {
-	    $(".nav .nav-link").each(function() {
-	      $(this).removeClass("active-nav-item");
-	    });
-	    $(this).addClass("active-nav-item");
-	    $(".nav .more").removeClass("active-nav-item");
-	  });
-	});
+  $(document).ready(function() {
+    var menu = $('#navigation-menu');
+    var menuToggle = $('#js-mobile-menu');
+    var signUp = $('.sign-up');
+  
+    $(menuToggle).on('click', function(e) {
+      e.preventDefault();
+      menu.slideToggle(function(){
+        if(menu.is(':hidden')) {
+          menu.removeAttr('style');
+        }
+      });
+    });
+  
+    // underline under the active nav item
+    $(".nav .nav-link").click(function() {
+      $(".nav .nav-link").each(function() {
+        $(this).removeClass("active-nav-item");
+      });
+      $(this).addClass("active-nav-item");
+      $(".nav .more").removeClass("active-nav-item");
+    });
+  });
 </script>
 
 </body>

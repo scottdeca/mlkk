9a549a5e4f40a8b871567c286cc3e87cae57d547
Scott Deca <decasc@gmail.com>
2014-06-17 15:30:50 -0400

    Wrote most of the html for the subpage

---
+++ subpage.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Home - MLK Kitchen for the Poor</title>
+  <link rel="stylesheet" href="css/app.css">
+  
+  <script type="text/javascript" src="//use.typekit.net/vvd8hgv.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+</head>
+<body>
+
+  <header class="navigation">
+    <div class="menu-wrapper">
+      <a href="javascript:void(0)" class="logo">
+        <img src="/img/kfp_logo_rev.png" alt="MLK Kitchen for the Poor">
+      </a>
+      <p class="navigation-menu-button" id="js-mobile-menu">MENU</p>
+      <div class="nav">
+        <ul id="navigation-menu">
+          <li class="nav-link"><a href="javascript:void(0)">Our Work</a></li>
+          <li class="nav-link"><a href="javascript:void(0)">Get Help</a></li>
+          <li class="nav-link"><a href="javascript:void(0)">Get Involved</a></li>
+          <li class="nav-link"><a href="javascript:void(0)">News & Events</a>
+          <li class="nav-link"><a href="javascript:void(0)">About Us</a></li>
+          <li class="nav-link"><a href="javascript:void(0)">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </header>
+
+  <div class="hero">
+    <div class="hero-inner">
+      <div class="hero-copy">
+        <h1>Tem qui dellor sunt voluptaepre 200 et a con 52 soluptati unt quo omniet occaesequo ommo.</h1>
+      </div>
+    </div>
+    <div class="overlay"></div>
+  </div>
+  
+  <section id="mission">
+    <h2>MLK Kitchen for the Poor feeds anyone who walks through its doors — regardless of race, age, sex or religious belief.</h2>
+    <p>The MLK Kitchen for the Poor was established in 1969 by the late Rev. Harvey Savage, Sr., in response to the tremendous need he saw in his community. Located in Toledo’s central city, the Kitchen feeds more than 200 people each day, from school children to the elderly.</p>
+    
+    <div class="mini-bullet-points 2-column">
+      <div class="bullet-point">
+        <h4>Meal delivery</h4>
+        <p>The MLK Kitchen for the Poor was established in 1969 by the late 
+Rev. the tremendous need he saw in his community.</p>
+      </div>
+      <div class="bullet-point">
+        <h4>Meal delivery</h4>
+        <p>The MLK Kitchen for the Poor was established in 1969 by the late 
+Rev. the tremendous need he saw in his community.</p>
+      </div>
+      <div class="bullet-point">
+        <h4>Meal delivery</h4>
+        <p>The MLK Kitchen for the Poor was established in 1969 by the late 
+Rev. the tremendous need he saw in his community.</p>
+      </div>
+      <div class="bullet-point">
+        <h4>Meal delivery</h4>
+        <p>The MLK Kitchen for the Poor was established in 1969 by the late 
+Rev. the tremendous need he saw in his community.</p>
+      </div>
+    </div>
+  </section>
+  
+  <section class="info-snacks">
+    <h3>Since opening our doors in 1969</h3>
+    <div class="sliding-facts">
+      <div id="slide-1">
+        <h4 class="big-number">2,309,239,001</h4>
+        <h5 class="description">Meals Served</h5>
+      </div>
+    </div>
+  </section>
+  
+  <section id="learn-about-hunger">
+    <h2>MLK Kitchen for the Poor feeds anyone who walks through its doors — regardless of race, age, sex or religious belief.</h2>
+    <p>The MLK Kitchen for the Poor was established in 1969 by the late Rev. Harvey Savage, Sr., in response to the tremendous need he saw in his community. Located in Toledo’s central city, the Kitchen feeds more than 200 people each day, from school children to the elderly.</p>
+    <p>The MLK Kitchen for the Poor was established in 1969 by the late Rev. Harvey Savage, Sr., in response to the tremendous need he saw in his community. Located in Toledo’s central city, the Kitchen feeds more than 200 people each day, from school children to the elderly.</p>
+  </section>
+  
+  <section class="quote">
+    <h3>“Were it not for the generous hearts of our donors and supporters, we would be unable to meet that need”</h3>
+    <span class="quote-name">Harvey</span>
+  </section>
+  
+  <section id="programs">
+    <h2>Lorem ipsum programs dolor mptium</h2>
+    <p>The MLK Kitchen for the Poor was established in 1969 by the late Rev. Harvey Savage, Sr., in response to the tremendous need he saw in his community. Located in Toledo’s central city, the Kitchen feeds more than 200 people each day, from school children to the elderly.</p>
+    
+    <div class="program-widget">
+      <div class="program-slide-1">
+        <div class="program-image">
+          <img src="http://placehold.it/618x256"
+        </div>
+        <div class="program-info">
+          <h3>Bowl-a-Thon</h3>
+          <p>The MLK Kitchen for the Poor was established in 1969 by the late Rev. Harvey Savage, Sr., in response to the tremendous need he saw in his community. Located in Toledo’s central city, the Kitchen feeds more than 200 people each day, from school children to the elderly.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  <section id="stories">
+    <h2>Lorem ipsum programs dolor mptium</h2>
+    <p>The MLK Kitchen for the Poor was established in 1969 by the late Rev. Harvey Savage, Sr., in response to the tremendous need he saw in his community. Located in Toledo’s central city, the Kitchen feeds more than 200 people each day, from school children to the elderly.</p>
+    
+    <div class="testimonial-widget">
+      <div id="story-slide-1">
+        <div class="testimonial-box">
+          <div class="testimonial-image">
+            <img src="http://placehold.it/300x250" alt="" />
+          </div>
+          <div class="testimonial-info">
+            <h4>Marc Reynolds: Nimre vendrae rem quas molutpta tioratu</h4>
+            <p>Testimonial about the the kitchenipsum Tas dion recte nim re vendae rem quas molupta tioratur explit et eossiti quisit rem fugit latem que pe.</p>
+          </div>
+        </div>
+        <button class="read-more">Continue reading &rarr;</span>
+      </div>
+      
+      <div id="story-slide-2">
+        <div class="testimonial-box">
+          <div class="testimonial-image">
+            <img src="http://placehold.it/300x250" alt="" />
+          </div>
+          <div class="testimonial-quote">
+            <p>Testimonial about the the kitchenipsum Tas dion recte nim re vendae rem quas molupta tioratur explit et eossiti quisit rem fugit latem que pe.</p>
+            <span class="testimonial-name">– Marc Reynolds</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  <footer>
+    <div class="grid-wrapper">
+      <div class="colophon">
+        <img class="logo" src="/img/kfp_logo_green.png" alt="MLK Kitchen for the Poor">
+        <div class="contact-info">
+          <ul class="address">
+            <li>Street Address</li>
+            <li>Address #1</li>
+            <li>Toledo, OH 43604</li>
+          </ul>
+          <ul class="phone-email">
+            <li>419 555 5555</li>
+            <li>contact@kitchenforthepoor</li>
+          </ul>
+        </div>
+        
+        <p class="copyright">Copyright ©2014 Martin Luther King Kitchen for the Poor. <a href="#">Privacy Policy</a></p>
+      </div>
+      
+      <div class="tell-us-more">
+        <h3>Tell us about you</h3>
+        <p>Whether you’d like to help out at our next event, help cook some meals, or just learn more, we’d be glad to hear from you.</p>
+        <form>
+          <select name="select_element">
+            <option value="1">Option 1</option>
+            <option value="2">Option 2</option>
+            <option value="3">Option 3</option>
+          </select>
+          <input class="button" type="submit" value="submit">
+        </form>
+      </div>
+    </div>
+  </footer>
+  
+
+<script src="js/vendor/jquery.js"></script>
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    var menu = $('#navigation-menu');
+    var menuToggle = $('#js-mobile-menu');
+    var signUp = $('.sign-up');
+  
+    $(menuToggle).on('click', function(e) {
+      e.preventDefault();
+      menu.slideToggle(function(){
+        if(menu.is(':hidden')) {
+          menu.removeAttr('style');
+        }
+      });
+    });
+  
+    // underline under the active nav item
+    $(".nav .nav-link").click(function() {
+      $(".nav .nav-link").each(function() {
+        $(this).removeClass("active-nav-item");
+      });
+      $(this).addClass("active-nav-item");
+      $(".nav .more").removeClass("active-nav-item");
+    });
+  });
+</script>
+
+</body>
+</html>

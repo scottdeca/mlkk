453ac7d4e74d3b68fab0916634d60fd2402e8708
Scott Deca <decasc@gmail.com>
2014-08-11 17:49:04 -0400

    Simplified get-help.php to the basics

--- parts/nav.php
+++ parts/nav.php
@@ -7,7 +7,7 @@
     <div class="nav">
       <ul id="navigation-menu">
         <li class="nav-link"><a href="/subpage.php">Our Work</a></li>
-        <li class="nav-link"><a href="javascript:void(0)">Get Help</a></li>
+        <li class="nav-link"><a href="/get-help.php">Get Help</a></li>
         <li class="nav-link"><a href="javascript:void(0)">Get Involved</a></li>
         <li class="nav-link"><a href="javascript:void(0)">News & Events</a>
         <li class="nav-link"><a href="javascript:void(0)">About Us</a></li>

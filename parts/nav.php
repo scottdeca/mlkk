d7b18418356c48fe7904980ebb75d201777bea14
Scott Deca <decasc@gmail.com>
2014-08-11 19:25:40 -0400

    Made second template for subpages — condensed

--- parts/nav.php
+++ parts/nav.php
@@ -6,8 +6,8 @@
     <p class="navigation-menu-button" id="js-mobile-menu">MENU</p>
     <div class="nav">
       <ul id="navigation-menu">
-        <li class="nav-link"><a href="/subpage.php">Our Work</a></li>
-        <li class="nav-link"><a href="/get-help.php">Get Help</a></li>
+        <li class="nav-link"><a href="/subpage-with-nav.php">Our Work</a></li>
+        <li class="nav-link"><a href="/subpage-condensed.php">Get Help</a></li>
         <li class="nav-link"><a href="javascript:void(0)">Get Involved</a></li>
         <li class="nav-link"><a href="javascript:void(0)">News & Events</a>
         <li class="nav-link"><a href="javascript:void(0)">About Us</a></li>
--- subpage.php
+++ subpage.php
@@ -8,7 +8,7 @@
   <script type="text/javascript" src="//use.typekit.net/vvd8hgv.js"></script>
   <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 </head>
-<body class="subpage subpage-with-subnav">
+<body class="subpage">
 
   <?php include("parts/nav.php"); ?>
 
@@ -22,310 +22,56 @@
   </div>
   
   
-  <div class="subnav-content">
+  <section id="mission">
+    <h2 class="section-name hidden">Mission</h2>
+    <h2>We offer hope and acceptance to those in need, keeping in mind the dignity of all persons.</h2>
+    <p>We help anyone who walks through our doors, regardless of age, sex, religion, political beliefs or race. We feed the homeless and those with low incomes. We provide hope where there seems to be none.</p>
+    
+    <p>Respect for each person, love for all &mdash; our values, simply put. We don’t just hand out food and clothes. We make sure to honor the people we serve by treating them with the respect they deserve.</p>
+  </section>
   
-    <section id="mission">
-      <h2 class="section-name hidden">Mission</h2>
-      <h2>We offer hope and acceptance to those in need, keeping in mind the dignity of all persons.</h2>
-      <p>We help anyone who walks through our doors, regardless of age, sex, religion, political beliefs or race. We feed the homeless and those with low incomes. We provide hope where there seems to be none.</p>
-      
-      <div class="mini-bullet-points 2-column">
-        <div class="mini-bullet-point">
-          <h4>Meal delivery</h4>
-          <p>Delivers meals to the elderly and area shut-ins.</p>
-        </div>
-        <div class="mini-bullet-point">
-          <h4>Meals for Summer Vacation</h4>
-          <p>Serves hot meals to neighborhood children during summer vacation</p>
-        </div>
-        <div class="mini-bullet-point">
-          <h4>Providing ingredients for home cooking</h4>
-          <p>Provides ingredients for food-stamp recipient families to prepare meals at home</p>
-        </div>
-        <div class="mini-bullet-point">
-          <h4>Extra help for the holidays</h4>
-          <p>Helps needy families with meals during the holidays</p>
-        </div>
-        <div class="mini-bullet-point">
-          <h4>Secret Santa, for the whole family</h4>
-          <p>Matches “adoptive” families with needy families to provide gifts for Christmas</p>
-        </div>
-        <div class="mini-bullet-point">
-          <h4>Providing essentials for our community</h4>
-          <p>Provides clothing, toys, counseling and support for the immediate community</p>
-        </div>
-        <div class="mini-bullet-point">
-          <h4>Christmas gifts for children</h4>
-          <p>Provides Christmas gifts to children</p>
-        </div>
-      </div>
-      
-      <p>Respect for each person, love for all &mdash; our values, simply put. We don’t just hand out food and clothes. We make sure to honor the people we serve by treating them with the respect they deserve.</p>
-    </section>
-    
-    <div class="info-snacks" style="background-image: url(/img/background-images/farmers.market_green.jpg);">
-      <div class="grid-wrapper">
-        <h3>Since opening our doors in 1969</h3>
-        <ul class="sliding-facts">
-          <li>
-            <h4 class="big-number">2,309,239,001</h4>
-            <h5 class="big-number-caption">Meals Served</h5>
-          </li>
-          <li>
-            <h4 class="big-number">2,309,239,001</h4>
-            <h5 class="big-number-caption">Meals Served</h5>
-          </li>
-          <li>
-            <h4 class="big-number">2,309,239,001</h4>
-            <h5 class="big-number-caption">Meals Served</h5>
-          </li>
-        </ul>
-      </div>
-    </div>
-    
-    <section id="learn-about-hunger" data-magellan-destination="hunger">
-      <h2 class="section-name hidden">Learn about Hunger</h2>
-      <h2>Recognizing that local hunger has global effects, we do what we can to feed the hungry.</h2>
-      <ul class="list-large">
-        <li>One out of six Americans does not have access to enough food</li>
-        <li>16 million children are living in food-insecure households</li>
-        <li>The lack of adequate nutrition affects the cognitive and behavioral development of children.</li>
-        <li>Children from food-insecure households are more likely to suffer from irritability, fatigue and difficulty concentrating than those who have adequate nutrition.</li>
-      </ul>
-      <cite>Based on statistics from <a href="#">Feed America</a></cite>
-    </section>
-    
-    <div class="featured-quote">
-      <div class="faded-background-image" style="background-image: url(/img/background-images/quote_bw.jpg);"></div>
-      <div class="grid-wrapper">
-        <h3>&ldquo;I have the audacity to believe that people everywhere can have three meals a day for their bodies, education and culture for the minds and dignity, equality and freedom for their spirits.&rdquo;
-</h3>
-        <cite class="quote-credit"><b>Martin Luther King, Jr.</b>, American Civil Rights Leader</cite>
-      </div>
-    </div>
-    
-    <section id="programs">
-      <h2 class="section-name hidden">Programs</h2>
-      <h2>Lorem ipsum programs dolor optium</h2>
-      <p>The MLK Kitchen for the Poor was established in 1969 by the late Rev. Harvey Savage, Sr., in response to the tremendous need he saw in his community. Located in Toledo’s central city, the Kitchen feeds more than 200 people each day, from school children to the elderly.</p>
-      
-      <div class="program-widget">
-        <article>
-          <div class="program-image">
-            <img src="http://placehold.it/800x256">
-          </div>
-          <div class="program-info">
-            <h3>Bowl-a-Thon</h3>
-            <p>The MLK Kitchen for the Poor was established in 1969 by the late Rev. Harvey Savage, Sr., in response to the tremendous need he saw in his community. Located in Toledo’s central city, the Kitchen feeds more than 200 people each day, from school children to the elderly.</p>
-          </div>
-        </article>
-        <article>
-          <div class="program-image">
-            <img src="http://placehold.it/800x256">
-          </div>
-          <div class="program-info">
-            <h3>Bowl-a-Thon</h3>
-            <p>The MLK Kitchen for the Poor was established in 1969 by the late Rev. Harvey Savage, Sr., in response to the tremendous need he saw in his community. Located in Toledo’s central city, the Kitchen feeds more than 200 people each day, from school children to the elderly.</p>
-          </div>
-        </article>
-        <article>
-          <div class="program-image">
-            <img src="http://placehold.it/800x256">
-          </div>
-          <div class="program-info">
-            <h3>Bowl-a-Thon</h3>
-            <p>The MLK Kitchen for the Poor was established in 1969 by the late Rev. Harvey Savage, Sr., in response to the tremendous need he saw in his community. Located in Toledo’s central city, the Kitchen feeds more than 200 people each day, from school children to the elderly.</p>
-          </div>
-        </article>
-      </div>
-    </section>
-    
-    
-    <section id="stories-slider">
-    
-      <h2 class="section-name hidden">Stories</h2>
-      <h2>Lorem ipsum programs dolor optium</h2>
-      <p>The MLK Kitchen for the Poor was established in 1969 by the late Rev. Harvey Savage, Sr., in response to the tremendous need he saw in his community. Located in Toledo’s central city, the Kitchen feeds more than 200 people each day, from school children to the elderly.</p>
-      
-    </section>
-    
-    <div class="testimonial-widget"><div class="grid-push-3">
-      <div id="testimonial-widget-slider">
-        
-        <article class="testimonial-box">
-          <div class="testimonial-image">
-            <img src="http://placehold.it/300x250" alt="" />
-          </div>
-          <div class="testimonial-quote">
-            <p>I am blessed. With my illness, it’s not easy. I count on the people at the kitchen to help me out every day. They make me feel loved and appreciated so I don’t feel isolated. I appreciate them and thank God for them every day.</p>
-            <cite class="testimonial-name">Cecelia</cite>
-          </div>
-        </article>
-        
-        <article class="testimonial-box">
-           <div class="testimonial-image">
-            <img src="http://placehold.it/300x250" alt="" />
-          </div>
-          <div class="testimonial-info">
-            <h4>&ldquo;I don’t know where I’d be without the help the kitchen gave us&rdquo;</h4>
-            <p>Willie Ward, Sr., principal of Martin Luther King, Jr., Academy for Boys in Toledo, is clear: the two biggest influences on his life to date are his mother and Martin Luther King, Jr...</p>
-            <div class="button-link-wrapper"><a href="#!" class="button-link buy-now button hollow">Continue reading &rarr;</a></div>
-          </div>
-        </article>
-        
-        <article class="testimonial-box">
-          <div class="testimonial-image">
-            <img src="http://placehold.it/300x250" alt="" />
-          </div>
-          <div class="testimonial-quote">
-            <p>Miss Henrietta and all the people at the kitchen have been so welcoming to us. We have been homeless and living in a shelter for a while. We always feel respected and loved when we come to the kitchen.</p>
-            <cite class="testimonial-name">Jeff</cite>
-          </div>
-        </article>
-        
-        <article class="testimonial-box">
-          <div class="testimonial-image">
-            <img src="http://placehold.it/300x250" alt="" />
-          </div>
-          <div class="testimonial-quote">
-            <p>I’ve been coming here for a long time. I don’t know what I would do without the kitchen. It’s a place where I don’t have to go hungry. I can count on coming here to get a decent lunch and whatever else they give.</p>
-            <cite class="testimonial-name">Joe</cite>
-          </div>
-        </article>
-        
-      </div>
-      <h2 class="product-name"></h2>
-    </div></div>
   
-  </div> <!-- .subnav-content ends -->
+  <section id="stories-slider">
+  
+    <h2 class="section-name hidden">Stories</h2>
+    <h2>Lorem ipsum programs dolor optium</h2>
+    <p>The MLK Kitchen for the Poor was established in 1969 by the late Rev. Harvey Savage, Sr., in response to the tremendous need he saw in his community. Located in Toledo’s central city, the Kitchen feeds more than 200 people each day, from school children to the elderly.</p>
+    
+  </section>
+  
+  
+
   
   
   <?php include("parts/footer.php"); ?>  
 
-<script src="js/vendor/jquery.js"></script>
-<script src="js/vendor/jquery.scrollNav.min.js"></script>
-
-<script src="js/vendor/slippry.js"></script>
-
-<script type="text/javascript">
-  jQuery('.sliding-facts').slippry({
-    // general elements & wrapper
-    slippryWrapper: '<div class="sy-box pictures-slider" />', // wrapper to wrap everything, including pager
+  <script src="js/vendor/jquery.js"></script>
   
-    // options
-    adaptiveHeight: false, // height of the sliders adapts to current slide
-    captions: false, // Position: overlay, below, custom, false
-  
-    // pager
-    pager: false,
+  <script type="text/javascript">
+    $(document).ready(function() {
+      var menu = $('#navigation-menu');
+      var menuToggle = $('#js-mobile-menu');
+      var signUp = $('.sign-up');
     
-    // controls
-    controls: false,
-    autoHover: false,
-  
-    // transitions
-    transition: 'horizontal', // fade, horizontal, kenburns, false
-    speed: 300 // time the transition takes (ms)
-  });
-  
-  jQuery('.program-widget').slippry({
-    // general elements & wrapper
-    slippryWrapper: '<div class="sy-box program-slider" />', // wrapper to wrap everything, including pager
-    elements: 'article',
+      $(menuToggle).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle(function(){
+          if(menu.is(':hidden')) {
+            menu.removeAttr('style');
+          }
+        });
+      });
     
-    // options
-    adaptiveHeight: true, // height of the sliders adapts to current slide
-    captions: false, // Position: overlay, below, custom, false
-  
-    // pager
-    pager: true,
-    
-    // controls
-    controls: false,
-    autoHover: true,
-  
-    // transitions
-    transition: 'horizontal', // fade, horizontal, kenburns, false
-    speed: 300, // time the transition takes (ms)
-    
-    // slideshow
-    auto: false,
-  });
-  
-  jQuery('#testimonial-widget-slider').slippry({
-    // general elements & wrapper
-    slippryWrapper: '<div class="sy-box testimonial-widget-slider" />', // wrapper to wrap everything, including pager
-    elements: 'article', // elments cointaining slide content
-  
-    // options
-    adaptiveHeight: false, // height of the sliders adapts to current slide
-    start: 2, // num (starting from 1), random
-    loop: false, // first -> last & last -> first arrows
-    captionsSrc: 'article',
-    //captions: 'custom', // Position: overlay, below, custom, false
-    captions: 'false',
-    captionsEl: '.product-name',
-  
-    // pager
-    pager: false,
-  
-    // transitions
-    slideMargin: 1, // spacing between slides (in %)
-    useCSS: true,
-    transition: 'horizontal',
-    easing: 'swing',
-    speed: 500, // time the transition takes (ms)
-  
-    // slideshow
-    auto: false
-  });
-  
-  $('.subnav-content').scrollNav({
-    sections: '.section-name',
-    subSections: false,
-    sectionElem: 'div',
-    showHeadline: false,
-    headlineText: 'Scroll To',
-    showTopLink: false,
-    topLinkText: 'Top',
-    fixedMargin: 40,
-    scrollOffset: 40,
-    animated: true,
-    speed: 500,
-    insertTarget: $('.subnav-content > section:first-child'), // this.selector
-    insertLocation: 'prependTo', //insertBefore, prependTo, appendTo, or insertAfter
-    arrowKeys: false,
-    onInit: null,
-    onRender: null,
-    onDestroy: null
-});
-</script>
-
-<script type="text/javascript">
-  $(document).ready(function() {
-    var menu = $('#navigation-menu');
-    var menuToggle = $('#js-mobile-menu');
-    var signUp = $('.sign-up');
-  
-    $(menuToggle).on('click', function(e) {
-      e.preventDefault();
-      menu.slideToggle(function(){
-        if(menu.is(':hidden')) {
-          menu.removeAttr('style');
-        }
+      // underline under the active nav item
+      $(".nav .nav-link").click(function() {
+        $(".nav .nav-link").each(function() {
+          $(this).removeClass("active-nav-item");
+        });
+        $(this).addClass("active-nav-item");
+        $(".nav .more").removeClass("active-nav-item");
       });
     });
-  
-    // underline under the active nav item
-    $(".nav .nav-link").click(function() {
-      $(".nav .nav-link").each(function() {
-        $(this).removeClass("active-nav-item");
-      });
-      $(this).addClass("active-nav-item");
-      $(".nav .more").removeClass("active-nav-item");
-    });
-  });
-</script>
+  </script>
 
 </body>
 </html>
